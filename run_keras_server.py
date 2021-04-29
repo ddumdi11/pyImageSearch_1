3772fcc6ecde6d7c3a421c69c9da3ced49b5363c
ddumdi11 <ddumdi11@gmail.com>
2021-04-29 18:56:50 +0200

    Änderung

--- run_keras_server.py
+++ run_keras_server.py
@@ -34,36 +34,36 @@ db = redis.StrictRedis(host="localhost", port=6379, db=0)
 model = None
 
 def base64_encode_image(a):
-	# base64 encode the input NumPy array
-	return base64.b64encode(a).decode("utf-8")
+    # base64 encode the input NumPy array
+    return base64.b64encode(a).decode("utf-8")
 
 def base64_decode_image(a, dtype, shape):
-	# if this is Python 3, we need the extra step of encoding the
-	# serialized NumPy string as a byte object
-	if sys.version_info.major == 3:
-		a = bytes(a, encoding="utf-8")
+    # if this is Python 3, we need the extra step of encoding the
+    # serialized NumPy string as a byte object
+    if sys.version_info.major == 3:
+        a = bytes(a, encoding="utf-8")
 
-	# convert the string to a NumPy array using the supplied data
-	# type and target shape
-	a = np.frombuffer(base64.decodestring(a), dtype=dtype)
-	a = a.reshape(shape)
+    # convert the string to a NumPy array using the supplied data
+    # type and target shape
+    a = np.frombuffer(base64.decodebytes(a), dtype=dtype)
+    a = a.reshape(shape)
 
-	# return the decoded image
-	return a
+    # return the decoded image
+    return a
 
 def prepare_image(image, target):
-	# if the image mode is not RGB, convert it
-	if image.mode != "RGB":
-		image = image.convert("RGB")
+    # if the image mode is not RGB, convert it
+    if image.mode != "RGB":
+        image = image.convert("RGB")
 
-	# resize the input image and preprocess it
-	image = image.resize(target)
-	image = img_to_array(image)
-	image = np.expand_dims(image, axis=0)
-	image = preprocess_input(image)
+    # resize the input image and preprocess it
+    image = image.resize(target)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = preprocess_input(image)
 
-	# return the processed image
-	return image
+    # return the processed image
+    return image
 
 def classify_process():
     # load the pre-trained Keras model (here we are using a model
@@ -136,50 +136,62 @@ def predict():
 
     # ensure an image was properly uploaded to our endpoint
     if flask.request.method == "POST":
-    if flask.request.files.get("image"):
-        # read the image in PIL format and prepare it for
-        # classification
-        image = flask.request.files["image"].read()
-        image = Image.open(io.BytesIO(image))
-        image = prepare_image(image, (IMAGE_WIDTH, IMAGE_HEIGHT))
+        if flask.request.files.get("image"):
+            # read the image in PIL format and prepare it for
+            # classification
+            image = flask.request.files["image"].read()
+            image = Image.open(io.BytesIO(image))
+            image = prepare_image(image, (IMAGE_WIDTH, IMAGE_HEIGHT))
 
-        # ensure our NumPy array is C-contiguous as well,
-        # otherwise we won't be able to serialize it
-        image = image.copy(order="C")
+            # ensure our NumPy array is C-contiguous as well,
+            # otherwise we won't be able to serialize it
+            image = image.copy(order="C")
 
-        # generate an ID for the classification then add the
-        # classification ID + image to the queue
-        k = str(uuid.uuid4())
-        d = {"id": k, "image": base64_encode_image(image)}
-        db.rpush(IMAGE_QUEUE, json.dumps(d))
+            # generate an ID for the classification then add the
+            # classification ID + image to the queue
+            k = str(uuid.uuid4())
+            d = {"id": k, "image": base64_encode_image(image)}
+            db.rpush(IMAGE_QUEUE, json.dumps(d))
 
-        # keep looping until our model server returns the output
-        # predictions
-        while True:
-            # attempt to grab the output predictions
-            output = db.get(k)
+            # keep looping until our model server returns the output
+            # predictions
+            while True:
+                # attempt to grab the output predictions
+                output = db.get(k)
 
-            # check to see if our model has classified the input
-            # image
-            if output is not None:
-                # add the output predictions to our data
-                # dictionary so we can return it to the client
-                output = output.decode("utf-8")
-                data["predictions"] = json.loads(output)
+                # check to see if our model has classified the input
+                # image
+                if output is not None:
+                    # add the output predictions to our data
+                    # dictionary so we can return it to the client
+                    output = output.decode("utf-8")
+                    data["predictions"] = json.loads(output)
 
-                # delete the result from the database and break
-                # from the polling loop
-                db.delete(k)
-                break
+                    # delete the result from the database and break
+                    # from the polling loop
+                    db.delete(k)
+                    break
 
-            # sleep for a small amount to give the model a chance
-            # to classify the input image
-            time.sleep(CLIENT_SLEEP)
+                # sleep for a small amount to give the model a chance
+                # to classify the input image
+                time.sleep(CLIENT_SLEEP)
 
-        # indicate that the request was a success
-        data["success"] = True
-        
-        # return the data dictionary as a JSON response
+            # indicate that the request was a success
+            data["success"] = True
+
+    # return the data dictionary as a JSON response
     return flask.jsonify(data)
 
+# if this is the main thread of execution first load the model and
+# then start the server
+if __name__ == "__main__":
+    # load the function used to classify input images in a *separate*
+    # thread than the one used for main classification
+    print("* Starting model service...")
+    t = Thread(target=classify_process, args=())
+    t.daemon = True
+    t.start()
 
+    # start the web server
+    print("* Starting web service...")
+    app.run()
